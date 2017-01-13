b69a0425551dbcb5ff57bf76e35905561e735038
thusharaprimus <thushara.sandakelum00@gmail.com>
2017-01-13 12:06:53 +0530

    logout

--- www/js/app.js
+++ www/js/app.js
@@ -77,5 +77,5 @@ angular.module('taggingApp', ['ionic', 'ionic-toast'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/login');
+  $urlRouterProvider.otherwise('/search');
 });
